3ebceef69a88c0f7337aa48e89eebd84c18ecea4
Nicolas Salazar <ngongorasalazar@godaddy.com>
2025-08-05 09:34:00 -0500

    fix: exports the uploadAssetToContentful action

--- packages/gastek-plugin-contentful-management/index.js
+++ packages/gastek-plugin-contentful-management/index.js
@@ -1,5 +1,7 @@
 import configure from "./configure.js";
+
 import { createContentfulEntry } from "./actions/create-contentful-entry.js";
+import { uploadAssetToContentful } from "./actions/upload-asset-to-contentful.js";
 
 const plugin = {
   name: "@gdex-standalone/gasket-plugin-contentful-management",
@@ -8,6 +10,7 @@ const plugin = {
   },
   actions: {
     createContentfulEntry,
+    uploadAssetToContentful,
   },
 };
 
